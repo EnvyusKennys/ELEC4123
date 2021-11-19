3ea7729e59fa6b54313efce8c2409a0632482809
YOKEN21 <kennyhan9831@gmail.com>
2021-11-20 00:19:13 +1100

    DONE!

--- client.py
+++ client.py
@@ -1,55 +1,79 @@
 # python3
 
 import socket
-import select
 import random
-import requests
-import codecs
 from struct import *
-from typing import List
-
 
 class Client():
 
     def __init__(self):
-        self.IP = '149.171.36.192'
-        self.local = '127.0.0.1'
-        self.port = 4000
-        self.snoop = 8189
-        self.http = 8190
-        self.pr = 100
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.setblocking(0)
+        self.host = '149.171.36.192'
+        self.snoop_port = 8189
 
     def Run(self):
+        # Creating socket [AF_INET: IPv4, SOCK_DGRAM: UDP]
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            # Connecting to the server
+            try:
+                s.connect((self.host,self.snoop_port))
+                print('----- Socket Connected! -----')
+            except s.error as err:
+                print('----- Socket Connection Failed! -----')
 
-        try:
-            self.sock.connect((self.IP, self.snoop))
-            print("Socket connected.")
+            # Sending snoop request & Receiving message from the server
+            # Sr is a random number between 10-20
+            Sr = random.randint(10, 20)
+            print('Sr is:', Sr)
+            msg_repeated = 0
+            Pr_l = []
+            id_l = []
+            id_l_int = []
+            msg_len_l = []
+            msg_dict = {}
+            # Send snoope request with differnt [Pr] to get differnt responses
+            for Pr in range(1,2000):
 
-            sr = 20
-            request = pack('!II', sr, self.pr)
-            print('Request message:', request)
+                # '!' -> Netwrok format which is Big-Endian; 'I' Unsigned integer which is 4 bytes each
+                print(f'----- Sr [{Sr}]: Receiving Message Pr No. [{Pr}] -----')
+                snoop_request = pack('!II', Sr, Pr) 
+                s.sendall(snoop_request)
+                res = s.recv(1024)
+                res_hex = res.hex()
+                # Get the same message for at least 5 times
+                if res_hex.endswith('04'):
+                    if msg_repeated == 3:
+                        print('----- At least 3 [\x04] received. Client Stop! ------\n')
+                        break
+                    else:
+                        msg_repeated+=1
 
-            self.sock.sendall(request)
-            ready = select.select([self.sock], [], [], 10)
-            if ready[0]:
-                # while True:
-                data = self.sock.recv(1024)
-                data = data.hex()
-                pr_rec = data[0:8]
-                msg_id = data[8:16]
-                msg = bytearray.fromhex(data[16:]).decode()
-                result = [int(msg_id, 16), msg]
-                print(result)
-                return result
-            else:
-                print('Timed out.')
+                # Get rid of duplicate response base on received Pr
+                rec_pr = res_hex[0:8]
+                if rec_pr in Pr_l:
+                    continue
+                else:
+                    Pr_l.append(rec_pr)
+                
+                # Store received messages in a dictionary [key: msg_identifier, value: msg]
+                msg = res[8:]
+                msg_len = len(msg)
+                msg_id = res_hex[8:16]
+                if msg in msg_dict.values():
+                    continue
+                else:
+                    id_l.append(msg_id)
+                    id_l_int.append(int(msg_id,16))
+                    msg_len_l.append(msg_len)
+                    msg_dict[msg_id] = msg
+            
+            # Closing socket
+            try:
+                s.close()
+                print('----- Socket Closed! -----')
+            except:
+                print('----- Socket Closing Failed! -----')
 
-        except Exception as err:
-            print('Socket closed')
-            self.sock.close()
+            return msg_dict
 
-
-c = Client()
-c.Run()
+# c = Client()
+# c.Run()
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from client import Client
 
 class Server():
     def __init__(self):
-        self.Pkt = []
+        self.msg_dict = {}
         self.httpStatus = 200
         self.IP = '149.171.36.192'
         self.http = 8190
@@ -15,20 +15,37 @@ class Server():
 
     def GetMessage(self):
         c = Client()
-        msg = c.Run()  # client request for once, appended in to the pkt list
-        self.Pkt.append(msg)
-
-        # check if EOF < 5 (create self.EOF_Pkts)
-        # Append self.variable
+        self.msg_dict = c.Run()
 
     def Reconstruct(self):
-        self.res_msg = ''
+        # Mod [msg_id] with [whole_message_length] to get the position of the this msg
+        pkt_per_msg = len(self.msg_dict)
+        # Msg_position is a dictionary with [key: msg_position, value: msg_id]
+        msg_position = {}
+        for id in self.msg_dict.keys():
+            msg_id_int = int(id,16)
+            # When pkt/msg is longer than msg_id, mod operation will not work
+            if pkt_per_msg > msg_id_int:
+                mod_id = msg_id_int - pkt_per_msg
+                msg_position[mod_id] = id
+            # When pkt/msg is shorter than msg_id, use mod operation
+            else:
+                mod_id = (msg_id_int)%(pkt_per_msg)
+                if mod_id == 0:
+                    mod_id = pkt_per_msg
+                msg_position[mod_id] = id
+        # Decode the message with 'utf-8'
+        for position in sorted(msg_position):
+            original_id = msg_position[position]
+            self.res_msg = self.res_msg + self.msg_dict[original_id].decode('utf-8')
+        self.res_msg = self.res_msg[self.res_msg.find('\x04')+1:] + self.res_msg[:self.res_msg.find('\x04')+1]
+        
         return self.res_msg
 
     def Post(self):
 
         while self.httpStatus == 200 or self.httpStatus == 406:
-
+            
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((self.IP, self.http))
             header = 'POST /session HTTP/1.1\r\n'
@@ -51,3 +68,8 @@ class Server():
                 print('Message revocered.')
             else:
                 print('Unexpected status.')
+
+control_server = Server()
+control_server.GetMessage()
+control_server.Reconstruct()
+control_server.Post()
