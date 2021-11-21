c1eb216e8b51ae56ba1a4d4b06c439ab7c2406a8
YOKEN21 <kennyhan9831@gmail.com>
2021-11-21 18:23:39 +1100

    Done!

--- client.py
+++ client.py
@@ -6,78 +6,26 @@ from struct import *
 
 class Client():
 
-    def __init__(self):
-        self.host = '149.171.36.192'
-        self.snoop_port = 8189
+    def __init__(self, client_socket, Sr, Pr, host, port):
+        self.Sr = Sr
+        self.Pr = Pr
+        self.client_socket = client_socket
+        self.snoop_host = host
+        self.snoop_port = port
 
     def Run(self):
-        # Creating socket [AF_INET: IPv4, SOCK_DGRAM: UDP]
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            # Connecting to the server
-            try:
-                s.connect((self.host,self.snoop_port))
-                print('---------- Socket Connected! ----------\n')
-            except s.error as err:
-                print('---------- Socket Connection Failed! ----------\n')
+        # Connecting to the server
+        try:
+            self.client_socket.connect((self.snoop_host,self.snoop_port))
+            print('---------- Socket Connected! ----------\n')
+        except self.client_socket.error as err:
+            print('---------- Socket Connection Failed! ----------\n')
 
-            # Sending snoop request & Receiving msg from server with Sr = random number between 10-20
-            Sr = random.randint(10, 20)
-            multi_dup_check = 0
-            msg_repeated = 0
-            Pr_l = []
-            id_l = []
-            id_l_int = []
-            msg_len_l = []
-            msg_dict = {}
-            # Send snoope request with differnt [Pr] to get differnt responses
-            for Pr in range(1,2000):
-
-                # '!' -> Netwrok format which is Big-Endian; 'I' Unsigned integer which is 4 bytes each
-                print(f'--- Sr [{Sr}]: Receiving Message Pr No. [{Pr}] ---')
-                snoop_request = pack('!II', Sr, Pr) 
-                s.sendall(snoop_request)
-                res = s.recv(1024)
-                res_hex = res.hex()
-                # Get the same message for at least 3 times to make sure all msg received
-                if res_hex.endswith('04'):
-                    if msg_repeated == 3:
-                        print('<<< SUCCESS: All Message Packets Received. Client Stop! >>>\n')
-                        break
-                    else:
-                        msg_repeated+=1
-
-                # Get rid of duplicate response base on received Pr
-                rec_pr = res_hex[0:8]
-                if rec_pr in Pr_l:
-                    continue
-                else:
-                    Pr_l.append(rec_pr)
-                
-                # Store received msg in a dictionary [key: msg_identifier, value: msg]
-                msg = res[8:]
-                msg_len = len(msg)
-                msg_id = res_hex[8:16]
-                # Get rid of duplicate msg
-                if msg in msg_dict.values():
-                    # If 10 consecutive dup msgs received -> Sr is a mutiple of pkt/msg -> change Sr
-                    multi_dup_check+=1
-                    if multi_dup_check == 10:
-                        print(f'<<< WARNING: Bad Sr [{Sr}] Detected. Sr Changed to [{Sr+1}] >>>\n')
-                        Sr+=1
-                    continue
-                else:
-                    # No consecutive dup msgs found, store msg
-                    multi_dup_check = 0
-                    id_l.append(msg_id)
-                    id_l_int.append(int(msg_id,16))
-                    msg_len_l.append(msg_len)
-                    msg_dict[msg_id] = msg
-            
-            # Closing socket
-            try:
-                s.close()
-                print('---------- Socket Closed! ----------\n')
-            except:
-                print('---------- Socket Closing Failed! ----------\n')
-
-            return msg_dict
+        # '!' -> Netwrok format which is Big-Endian; 'I' Unsigned integer which is 4 bytes each
+        print(f'--- Sr [{self.Sr}]: Receiving Message Pr No. [{self.Pr}] ---')
+        snoop_request = pack('!II', self.Sr, self.Pr) 
+        self.client_socket.sendall(snoop_request)
+        res = self.client_socket.recv(1024)
+        
+        return res
+        
--- server.py
+++ server.py
@@ -1,20 +1,60 @@
 # draft for control server
 
 import socket
+import random
 import requests
 from client import Client
 
 class Server():
     def __init__(self):
-        self.msg_dict = {}
-        self.httpStatus = 200
         self.IP = '149.171.36.192'
         self.http = 8190
+        self.snoop = 8189
+        self.httpStatus = 200
+        self.Sr = 0
+        self.Pr = 0
+        self.msg_no = 1
+        self.msg_repeated = 0
+        self.multi_dup_check = 0
+        self.Pr_l = []
+        self.msg_dict = {}
         self.res_msg = ''
 
-    def GetMessage(self):
-        c = Client()
-        self.msg_dict = c.Run()
+    def GetMessage(self, client_socket, Sr, Pr):
+        # Send snoope request with differnt [Pr] to get differnt responses
+        for Pr in range(1,2000):
+            # One client connet and get one response
+            client1 = Client(client_socket, Sr, Pr, self.IP, self.snoop)
+            res = client1.Run()
+            res_hex = res.hex()
+            # Get the same message for at least 3 times to make sure all msg received
+            if res_hex.endswith('04'):
+                if self.msg_repeated == 3:
+                    print('<<< SUCCESS: All Message Packets Received. Client Stop! >>>\n')
+                    break
+                else:
+                    self.msg_repeated+=1
+            # Get rid of duplicate response base on received Pr
+            rec_pr = res_hex[0:8]
+            if rec_pr in self.Pr_l:
+                continue
+            else:
+                self.Pr_l.append(rec_pr) 
+            # Store received msg in a dictionary [key: msg_identifier, value: msg]
+            msg = res[8:]
+            msg_id = res_hex[8:16]
+            # Get rid of duplicate msg
+            if msg in self.msg_dict.values():
+                # If 10 consecutive dup msgs received -> Sr is a mutiple of pkt/msg -> change Sr
+                multi_dup_check+=1
+                if multi_dup_check == 10:
+                    print(f'<<< WARNING: Bad Sr [{Sr}] Detected. Sr Changed to [{Sr+1}] >>>\n')
+                    Sr+=1
+                continue
+            else:
+                # No consecutive dup msgs found, store msg
+                multi_dup_check = 0
+                self.msg_dict[msg_id] = msg
         
     def Reconstruct(self):
         # Mod [msg_id] with [whole_message_length] to get the position of the this msg
@@ -41,34 +81,59 @@ class Server():
         
         return self.res_msg
 
-    def Post(self):
+    def Run(self):
+        # Creating socket for clients [AF_INET: IPv4, SOCK_DGRAM: UDP]
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+            while self.httpStatus == 200 or self.httpStatus == 406:
+                print(f'---------- Recovering Message No. [{self.msg_no}] ----------\n')
+                # Get new message and reconstruct the message
+                self.Sr = random.randint(10, 20)
+                self.Pr = 0
+                self.msg_repeated = 0
+                self.multi_dup_check = 0
+                self.Pr_l = []
+                self.msg_dict = {}
+                self.res_msg = ''
+                self.GetMessage(client_socket, self.Sr, self.Pr)
+                self.Reconstruct()
 
-        while self.httpStatus == 200 or self.httpStatus == 406:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((self.IP, self.http))
+                header = 'POST /session HTTP/1.1\r\n'
+                host = 'HOST: ' + self.IP + ":8190\r\n"
+                contentlength = 'Content-Length: ' + \
+                    str(len(self.res_msg)) + '\r\n\r\n'
+                data = header + host + contentlength + self.res_msg
 
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((self.IP, self.http))
-            header = 'POST /session HTTP/1.1\r\n'
-            host = 'HOST: ' + self.IP + ":8190\r\n"
-            contentlength = 'Content-Length: ' + \
-                str(len(self.res_msg)) + '\r\n\r\n'
-            data = header + host + contentlength + self.res_msg
+                s.sendall(str.encode(data))
+                status = s.recv(4096)
 
-            s.sendall(str.encode(data))
-            status = s.recv(4096)
-
-            if "200" in status.decode():
-                httpStatus = 200
-                print('Post success.')
-            elif "406" in status.decode():
-                httpStatus = 406
-                print('Post failed.')
-            elif "205" in status.decode():
-                httpStatus = 205
-                print('Message recovered.')
-            else:
-                print('Unexpected status.')
+                if "200" in status.decode():
+                    httpStatus = 200
+                    print('Post success.')
+                elif "406" in status.decode():
+                    httpStatus = 406
+                    print('Post failed.')
+                elif "205" in status.decode():
+                    httpStatus = 205
+                    print('Message recovered.')
+                else:
+                    print('Unexpected status.')
+                    break
+                self.msg_no+=1
+            
+            # Closing client socket
+            try:
+                client_socket.close()
+                print('---------- Client Socket Closed! ----------\n')
+            except:
+                print('---------- Client Socket Closing Failed! ----------\n')
+            # Clsoing http server socket
+            try:
+                s.close()
+                print('---------- HTTP Server Socket Closed! ----------\n')
+            except:
+                print('---------- HTTP Server Socket Closing Failed! ----------\n')
 
 control_server = Server()
-control_server.GetMessage()
-control_server.Reconstruct()
-control_server.Post()
+control_server.Run()
